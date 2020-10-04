6ff7a27074db38bba55550c89c6f8868be682a80
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-04 19:20:43 +0200

    L03 Ex02

---
+++ modulo-2-leccion-03/Ejercicio 02/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = "Nuria";
+
+if (name === "Nuria") {
+  console.log("Bienvenida Nuria");
+} else {
+  console.log("Lo siento pero el usuario que has introducido no está registrado");
+}
