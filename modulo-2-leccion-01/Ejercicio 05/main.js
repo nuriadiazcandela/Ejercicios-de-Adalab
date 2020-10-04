c2b295fa262470e116b5699ba65af9839492e153
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-04 16:29:28 +0200

    L02 Ex05

---
+++ modulo-2-leccion-01/Ejercicio 05/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const title = document.querySelector(".title");
+const pasword = document.querySelector(".pasword");
+pasword.innerHTML = "**";
+title.innerHTML = title.innerHTML + pasword.innerHTML;
