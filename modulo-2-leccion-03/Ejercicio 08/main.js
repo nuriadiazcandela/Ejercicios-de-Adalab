484e80d32cf0e5129fbb0f1f2d1020e595f6e845
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-05 19:36:15 +0200

    L04 Ex1

---
+++ modulo-2-leccion-03/Ejercicio 08/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const currentYear = 34;
+const yearsSinceLeapYear = currentYear % 4;
+let nextLeapYear = "";
+
+switch (yearsSinceLeapYear) {
+  case 1:
+    nextLeapYear = currentYear + 3;
+    console.log(`El próximo año bisiesto es el ${nextLeapYear}`);
+    console.log(`1`);
+    break;
+  case 2:
+    nextLeapYear = currentYear + 2;
+    console.log(`El próximo año bisiesto es el ${nextLeapYear}`);
+    console.log(`2`);
+    break;
+  case 3:
+    nextLeapYear = currentYear + 1;
+    console.log(`El próximo año bisiesto es el ${nextLeapYear}`);
+    console.log(`3`);
+    break;
+  default:
+    console.log(`Este año es un año bisiesto`);
+}
---
+++ modulo-2-leccion-04/Ejercicio-01/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function mult(a, b) {
+  const result = a * b;
+  return result;
+}
+
+console.log(mult(5, 6));
