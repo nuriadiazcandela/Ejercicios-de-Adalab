a06ef260079ec0572843962ba01a57bb4bdce652
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-06 19:58:25 +0200

    L01 Ex6

--- modulo-2-leccion-01/Ejercicio 06/main.js
+++ modulo-2-leccion-01/Ejercicio 06/main.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const element = document.querySelector(`.div`);
+const content = `<h1> Lorem ipsum</h1> <img src = http://via.placeholder.com/350x150 /img> <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit`;
+
+element.innerHTML = content;
