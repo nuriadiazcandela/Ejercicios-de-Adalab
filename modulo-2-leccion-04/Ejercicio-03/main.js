38ff26310f53cc463f87d079af77b76a8780f6b2
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-06 10:05:38 +0200

    L04 Ex3

--- modulo-2-leccion-04/Ejercicio-03/main.js
+++ modulo-2-leccion-04/Ejercicio-03/main.js
@@ -3,6 +3,7 @@
 function precioIva(precio) {
   const iva = precio * 0.21;
   const total = precio + iva;
-  return `Precio sin IVA:` + precio, `IVA: ` + iva, `Total:` + total;
+  console.log(`El precio es ${precio}, el IVA es ${iva}, el total es ${total}`);
 }
-console.log(precioIva(10));
+
+precioIva(10);
