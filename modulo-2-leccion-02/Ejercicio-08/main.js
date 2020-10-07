1d91f77a5a482f7a16920208c48ca42e7e8829f7
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-07 19:13:47 +0200

    L02 Ex8

--- modulo-2-leccion-02/Ejercicio-08/main.js
+++ modulo-2-leccion-02/Ejercicio-08/main.js
@@ -1,5 +1,4 @@
 "use strict";
-const firstDog
 
 const firstDogImage = "https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg";
 const firstDogName = "Dina";
@@ -10,8 +9,5 @@ const secondDogName = "Luna";
 const thirdDogImage = "https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg";
 const thirdDogName = "Lana";
 
-firstDogImage.innerHTML = (
-  <ul>
-    <li>${firstDogName}</li>
-  </ul>
-);
+let ul = document.querySelector("ul");
+ul.innerHTML = `<li><h1>${firstDogName}</h1><img src="${firstDogImage}"</li> <li><h1>${secondDogName}</h1><img src="${secondDogImage}"</li><li><h1>${thirdDogName}</h1><img src="${thirdDogImage}"</li>`;
