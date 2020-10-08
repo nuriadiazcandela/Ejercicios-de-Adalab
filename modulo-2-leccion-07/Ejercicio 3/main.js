61506a21ad35a07b6d3b9b45106d6f0541965e91
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-08 19:16:37 +0200

    L07 Ex3

---
+++ modulo-2-leccion-07/Ejercicio 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let acc = 0;
+for (acc = 0; acc < 11; acc = acc + 2) {
+  console.log(`voy por la vuelta ` + acc);
+}
+//me falta el final del ejercicio Mostrar el resultado es : X, siendo X el valor de la variable acc
