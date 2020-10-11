064c7024084c9ae9d3d2ab395d7560f8dda5cbfc
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-11 12:34:36 +0200

    L03 Ex7

---
+++ modulo-2-leccion-03/Ejercicio 07/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//let ingredient = 'Pollo';
+//let ingredient = 'Merluza'
+let ingredient = '';
+
+if (ingredient === 'Pollo') {
+    console.log ('El ingrediente elegido es Pollo, le sugerimos Filete con patatas');
+}else if (ingredient === 'Merluza') {
+    console.log ('El ingrediente elegido es Merluza, le sugerimos Merluzita en salsa verde');
+}else if (ingredient === 'Espinacas') {
+    console.log ('El ingrediente elegido es Espinacas, le sugerimos espinacas rehogadas');
+}else{
+ console.log ('Nada en la nevera');
+}
+
+ 
