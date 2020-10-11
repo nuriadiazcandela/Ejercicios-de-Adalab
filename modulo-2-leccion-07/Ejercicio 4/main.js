fe6d99c659ca2bc45823aa530ddc2bfdf0bec21b
Nuria Diaz <nuriadiazcandela@gmail.com>
2020-10-11 12:13:32 +0200

    L03 Ex5

--- modulo-2-leccion-07/Ejercicio 4/main.js
+++ modulo-2-leccion-07/Ejercicio 4/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 let moon = 2017;
-for (moon = 2017; moon < 2035; moon = moon + 3) {
-  console.log(`La próxima Luna del cazador será el año ` + moon);
+
+for (let i = 1; i <= 15; i++) {
+  moon += 3;
+  console.log("La próxima Luna del cazador será el año " + moon);
 }
-// me falta que salgan las proximas 15 lunas (ver si hay otra forma en vez de calcularlo yo a mano)
