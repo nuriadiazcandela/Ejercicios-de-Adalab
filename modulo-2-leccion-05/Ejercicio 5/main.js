60b0b7ad40e5c812981019822e8139ef819074b7
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-12 17:41:51 +0200

    L05 Ex5

--- modulo-2-leccion-05/Ejercicio 5/main.js
+++ modulo-2-leccion-05/Ejercicio 5/main.js
@@ -1,14 +1,16 @@
 "user strict";
 
-const container = document.querySelectorAll (".body")
+const container = document.querySelector(".container")
 
 function changeColor(event){
     if (event.key === "r"){
-        container.classList.add("red");
-        container.classList.remove("purple");
+      container.classList.add ("red")
+      container.classList.remove ("purple")
+       
     } else if (event.key === "m"){
-        container.classList.add("purple");
-        container.classList.remove("red");
+        container.classList.add ("purple")
+        container.classList.remove ("red")
+        
     }
 }
 document.addEventListener("keydown", changeColor);
