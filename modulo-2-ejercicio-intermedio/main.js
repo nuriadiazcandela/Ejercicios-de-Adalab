f38380abfb2e2a65738ea5c43a028df4aa5225ea
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-12 17:36:57 +0200

    Repaso evaluacion intermedia modulo 2

--- modulo-2-ejercicio-intermedio/main.js
+++ modulo-2-ejercicio-intermedio/main.js
@@ -1,12 +1,22 @@
 "use strict";
 
-const smile = document.querySelector(".js-face");
+const smile = document.querySelector(".js-smile");
 const button = document.querySelector(".js-btn");
 const select = document.querySelector(".js-select")
+const container = document.querySelector(".js-container")
 
 function updateclick() {
- const selectValue = select.value;
- smile.innerHTML = selectValue;
+    const selectValue = select.value;
+    smile.innerHTML = selectValue;
+    const random = parseInt(Math.random()*100);
+    if (random % 2 === 0) {
+        container.classList.add("yelow");
+        container.classList.remove("orange");
+    }else {
+        container.classList.remove("yelow");
+        container.classList.add("orange");
+    }
 }
 
 button.addEventListener("click", updateclick);
+
