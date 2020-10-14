28384579f531d985cab692d2d85d20c99ea28846
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-14 16:36:52 +0200

    L09 Ex1

---
+++ modulo-2-leccion-09/Ejercicio 1/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function get100Numbers () {
+    const arrayNumbers = []
+    for (let i=1; i <=100; i++) {
+    arrayNumbers.push(i);
+    console.log(arrayNumbers);
+    }  
+}
+
+get100Numbers()
