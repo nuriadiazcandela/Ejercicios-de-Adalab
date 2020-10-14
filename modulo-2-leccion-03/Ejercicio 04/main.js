4650a9287cbc6ba9da043a51202d40f409529158
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-14 19:01:18 +0200

    L03 Ex4

--- modulo-2-leccion-03/Ejercicio 04/main.js
+++ modulo-2-leccion-03/Ejercicio 04/main.js
@@ -1,1 +1,15 @@
 "user strict";
+
+let ageDog = 4;
+const ageOne = 15;
+const ageTwo = 9;
+const agePlus = 5;
+
+
+if (ageDog === 1) {
+    console.log (`La edad de tu perro equivale a `+ ageOne + ` años de humano`)
+} else if (ageDog === 2) { 
+    console.log (`La edad de tu perro equivale a ${ageOne + ageTwo} años de humano`)
+} else {
+    console.log (`La edad de tu perro equivale a ${ageOne + ageTwo + (ageDog - 2)* agePlus} años de humano`)
+}
---
+++ modulo-2-leccion-09/Ejercicio 3/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+const lostPair = [];
+
+function bestLostNomber () {
+ if (lostNumbers  % 2) {
+     lostPair = lostNumbers;
+     
+ }
+}
