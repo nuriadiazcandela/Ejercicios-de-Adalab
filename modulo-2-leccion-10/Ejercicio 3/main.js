ecd95e71be83217ab286749e88a823fef6a3404d
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-16 19:01:50 +0200

    L10 Ex3

--- modulo-2-leccion-10/Ejercicio 3/main.js
+++ modulo-2-leccion-10/Ejercicio 3/main.js
@@ -1,11 +1,20 @@
-function getDogImage() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then(data => {
-        const img = document.querySelector("img");
-        img.src = data.message;
-        img.alt = "Un perro";
-      });
-  }
-  const btn = document.querySelector(".js-dog");
-  btn.addEventListener("click", getDogImage);
+"use strict";
+
+ function getUser() {
+   const inputValue = document.querySelector(".text").value;
+  
+   fetch("https://api.github.com/users/" + inputValue)
+       .then(response => response.json())
+       .then(data => {
+        
+        const name = document.querySelector(".name")
+        name.innerHTML = data.name;
+        const img = document.querySelector(".img")
+        img.src = data.avatar_url;
+        const number = document.querySelector(".number")
+        number.innerHTML = data.public_repos;
+             });
+   }
+   const btn = document.querySelector(".js-btn");
+   btn.addEventListener("click", getUser);
+
