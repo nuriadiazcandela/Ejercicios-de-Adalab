79a8e97f53e186a5a274e7abcdab270263896221
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-16 09:17:22 +0200

    L10 Ex1

--- modulo-2-leccion-10/Ejercicio 1/main.js
+++ modulo-2-leccion-10/Ejercicio 1/main.js
@@ -1,10 +1,10 @@
 "user strict";
 
-  function getEmoji() {
+  function getNumber() {
     fetch("https://api.rand.fun/number/integer")
       .then(response => response.json())
       .then(data => {
         document.querySelector('.js-result').innerHTML = data.result
       });
   }
-  document.querySelector('.js-emoji').addEventListener("click", getEmoji);
+  document.querySelector('.js-numbers').addEventListener("click", getNumber);
