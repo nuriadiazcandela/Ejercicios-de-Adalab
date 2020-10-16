3ec8a56d64553449bfc95752afb3c912136f2e86
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-16 19:27:54 +0200

    L10 Ex4

--- modulo-2-leccion-10/Ejercicio 4/main.js
+++ modulo-2-leccion-10/Ejercicio 4/main.js
@@ -1,11 +1,22 @@
-function getDogImage() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then(data => {
-        const img = document.querySelector("img");
-        img.src = data.message;
-        img.alt = "Un perro";
+"use strict";
+
+function getRepos() {
+  const inputValue = document.querySelector(".js-text").value
+    fetch("https://api.github.com/orgs/" + inputValue)
+      .then(firstResponse => firstResponse.json())
+      .then(firstData => {
+        const repos = firstData.repos_url;
+        console.log(repos)
+        return fetch (repos);
+      })
+      .then(secondResponse => secondResponse.json())
+      .then(secondData => {
+        const list = document.querySelector(".list");
+        for (let i= 0; i < secondData.length; i++) {
+          list.innerHTML +=`<li>${secondData[i].name}</li>`
+        }
       });
+
   }
-  const btn = document.querySelector(".js-dog");
-  btn.addEventListener("click", getDogImage);
+  const btn = document.querySelector(".js-button");
+  btn.addEventListener("click", getRepos);
