a308fe21847a7001ece3c3bc249846778170c40a
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-21 19:06:04 +0200

    L07 Ex5

--- Módulo-2/modulo-2-leccion-07/Ejercicio 5/main.js
+++ Módulo-2/modulo-2-leccion-07/Ejercicio 5/main.js
@@ -1,4 +1,14 @@
 "use strict";
 
 const numbers = [5, 22, 10, 4, 18];
-acc = 
+const numbers2 = [5, 22, 10, 4, 18, 56];
+let acc = 0;
+
+function media(array) {
+  for (let i = 0; i < array.length; i++) {
+    acc += array[i];
+  }
+  return "La media es igual a: " + acc / array.length;
+}
+console.log(media(numbers));
+console.log(media(numbers2));
---
+++ Módulo-2/modulo-2-leccion-13/Ejercicio 3/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const users = [
+  { name: "María", isPremium: false },
+  { name: "Lucía", isPremium: true },
+  { name: "Susana", isPremium: true },
+  { name: "Rocío", isPremium: false },
+  { name: "Inmaculada", isPremium: false },
+];
