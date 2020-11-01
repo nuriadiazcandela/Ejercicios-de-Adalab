5c5c51b3be999233e9e4ff01634192815318653c
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-11-01 18:15:05 +0100

    L02 Ex 1

--- Módulo-3/modulo-3-leccion-02/Ejercicio 1/my-react-project/src/App.js
+++ Módulo-3/modulo-3-leccion-02/Ejercicio 1/my-react-project/src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
+import foto from './images.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>¡Hola, mundo!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Mediacard">
+      <header className="Card-header">
+        <img src={foto} className="App-card" alt="foto" />
+        <div>
+          <h1 className="Card-h1">Alex Guerrero</h1>
+          <p>Domingo, 1 de noviembre de 2020</p>
+        </div>
       </header>
+      <main>
+        <p>
+          Lorem fistrum la caidita por la gloria de mi madre de la pradera por la gloria de mi madre
+          a peich papaar papaar va usté muy cargadoo apetecan está la cosa muy malar. Mamaar te voy
+          a borrar el cerito sexuarl está la cosa muy malar tiene musho peligro torpedo. Fistro por
+          la gloria de mi madre sexuarl caballo blanco caballo negroorl pecador a gramenawer mamaar
+          llevame al sircoo ese pedazo de te voy a borrar el cerito a gramenawer. Pupita a wan
+          pupita ahorarr. Ese pedazo de papaar papaar a wan ese pedazo de te va a hasé pupitaa
+          quietooor mamaar pecador. Al ataquerl ese que llega pupita fistro la caidita. Ese hombree
+          benemeritaar condemor diodeno pecador condemor te va a hasé pupitaa me cago en tus muelas
+          diodenoo de la pradera tiene musho peligro.
+        </p>
+        <p className="heart"> ❤︎</p>
+      </main>
     </div>
   );
 }
