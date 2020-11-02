228fe3ea8c617b011d667d1d0aaadb0da0c030f2
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-11-02 11:11:56 +0100

    L01 Ex Bonus

---
+++ Módulo-3/modulo-3-leccion-01/Ejercicio Bonus A/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Polygon {
+  constructor(numberOfSides, base, height) {
+    this.numberOfSides = numberOfSides;
+    this.base = base;
+    this.height = height;
+  }
+  perimeter() {
+    return this.base * this.numberOfSides;
+  }
+  area() {
+    return this.base * this.height;
+  }
+}
+class Square extends Polygon {
+  constructor(side) {
+    super(4, side, side);
+  }
+}
+class Triangle extends Polygon {
+  constructor(base, height) {
+    super(3, base, height);
+  }
+  area() {
+    return super.area() / 2;
+  }
+}
+const oneSquare = new Square(4);
+const oneTriangle = new Triangle(4, 3);
+
+console.log(oneSquare.perimeter());
+console.log(oneSquare.area());
+
+console.log(oneTriangle.perimeter());
+console.log(oneTriangle.area());
