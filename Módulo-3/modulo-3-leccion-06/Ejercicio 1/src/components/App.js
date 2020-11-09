6abb3e60ba771fb99257acf9f53c9019dd648fa6
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-11-09 16:37:10 +0100

    L06 Ex1

---
+++ Módulo-3/modulo-3-leccion-06/Ejercicio 1/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../stylesheet/App.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+    this.handleKeyup = this.handleKeyup.bind(this);
+  }
+  handleKeyup(event) {
+    this.setState({
+      inputText: event.currentTarget.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" onKeyUp={this.handleKeyup} />
+        <p>{this.state.inputText}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
