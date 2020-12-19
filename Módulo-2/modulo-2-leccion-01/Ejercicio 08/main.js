7c87b629fb5aeeac3092a9765c688c90d980e8dc
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-12-19 18:22:06 +0100

    finish Ex08

--- Módulo-2/modulo-2-leccion-01/Ejercicio 08/main.js
+++ Módulo-2/modulo-2-leccion-01/Ejercicio 08/main.js
@@ -1,7 +1,4 @@
-"use strict";
+'use strict';
 
-const buttonTwo = document.querySelector(`button-two`);
-
+const buttonTwo = document.querySelector(`.button-two`);
 buttonTwo.classList.add(`opacity`);
-
-//SIN ACABAR, ESTA MAL
