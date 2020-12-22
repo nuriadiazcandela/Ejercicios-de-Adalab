830dc06e7f0cc84a0960eac0766d7439a7725d68
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-12-22 10:33:17 +0100

    L06 Ex7

---
+++ Módulo-2/modulo-2-leccion-06/Ejercicio 7/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const job = 'developer';
+const object = {};
+
+object.firstName = 'Nuria';
+object.lastName = 'Diaz';
+object.age = 34;
+object.job = 'developer';
+
+console.log(object.firstName);
+console.log(object.lastName);
+console.log(object.age);
+console.log(object.job);
+
+object.firstName = 'Maria';
+object.age = 35;
+
+console.log(object.firstName);
+console.log(object.lastName);
+console.log(object.age);
+console.log(object.job);
