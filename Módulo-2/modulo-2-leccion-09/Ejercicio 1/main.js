1294f760ab393e62a0639bcf1942457b9c3c41d5
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-01-10 10:58:20 +0100

    fix L09 Ex1

--- Módulo-2/modulo-2-leccion-09/Ejercicio 1/main.js
+++ Módulo-2/modulo-2-leccion-09/Ejercicio 1/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function get100Numbers () {
-    const arrayNumbers = []
-    for (let i=1; i <=100; i++) {
+function get100Numbers() {
+  const arrayNumbers = [];
+  for (let i = 1; i <= 100; i++) {
     arrayNumbers.push(i);
     console.log(arrayNumbers);
-    }  
+  }
 }
 
-get100Numbers()
+get100Numbers();
