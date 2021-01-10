462aa4e3fa31c5c09cb9615de811a5cc3461a07e
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-01-10 10:59:46 +0100

    fix L09 Ex2

--- Módulo-2/modulo-2-leccion-09/Ejercicio 2/main.js
+++ Módulo-2/modulo-2-leccion-09/Ejercicio 2/main.js
@@ -1,17 +1,16 @@
-"use strict";
+'use strict';
 
-function get100Numbers () {
-    const arrayNumbers = []
-    for (let i=1; i <=100; i++) {
+function get100Numbers() {
+  const arrayNumbers = [];
+  for (let i = 1; i <= 100; i++) {
     arrayNumbers.push(i);
-    console.log(arrayNumbers);
-    }
-    return arrayNumbers
+  }
+  console.log(arrayNumbers);
+  return arrayNumbers;
 }
 
-
-function getReversed100Numbers () {
-    let arrayReverse = get100Numbers()
-    console.log(arrayReverse.reverse())
+function getReversed100Numbers() {
+  let arrayReverse = get100Numbers();
+  console.log(arrayReverse.reverse());
 }
-getReversed100Numbers()
+getReversed100Numbers();
