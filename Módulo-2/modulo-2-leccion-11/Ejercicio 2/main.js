b021ffe8a4e7d771adcb88f154c8cf6f2e8847df
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-01-12 09:36:57 +0100

    L11 Ex2

--- Módulo-2/modulo-2-leccion-11/Ejercicio 2/main.js
+++ Módulo-2/modulo-2-leccion-11/Ejercicio 2/main.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-const btn = document.querySelector(".js-btn");
+const btn = document.querySelector('.js-btn');
 
 function getCharacter() {
-  const character = document.querySelector(".js-name").value;
+  const character = document.querySelector('.js-name').value;
   console.log(character);
   fetch(`https://swapi.dev/api/people/?search=${character}`)
     .then((response) => response.json())
     .then((data) => {
-      let result = document.querySelector(".js-result");
-      result.innerHTML = `<li> Personaje: ${data.results[0].name}</li>`;
+      let result = document.querySelector('.js-result');
+      result.innerHTML = `<li> Personaje: ${data.results[0].name}</li> <li> Género: ${data.results[0].gender}</li>`;
     });
 }
-btn.addEventListener("click", getCharacter);
+btn.addEventListener('click', getCharacter);
