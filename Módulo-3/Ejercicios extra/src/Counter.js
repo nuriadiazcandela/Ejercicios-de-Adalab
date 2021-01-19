3da2f82665ed8d17b0ec24b7c0b62b2eabaa1894
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-01-19 17:29:24 +0100

    add module 4

--- Módulo-3/Ejercicios extra/src/Counter.js
+++ Módulo-3/Ejercicios extra/src/Counter.js
@@ -3,18 +3,18 @@ import React from 'react';
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 0 };
+    this.state = { counter: 0 };
     this.buttonClicked = this.buttonClicked.bind(this);
   }
   buttonClicked() {
-    this.setState({ value: this.state.value + 1 });
+    this.setState({ counter: this.state.counter + 1 });
   }
 
   render() {
     return (
       <div>
         <button onClick={this.buttonClicked}>Click</button>
-        <div>{this.state.value}</div>
+        <div>{this.state.counter}</div>
       </div>
     );
   }
