def9190881f10ea49dd5aa89e32cea2eb2b119a7
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-02-02 19:26:20 +0100

    ex Paletas

--- Módulo-2/Ejercicios-extra-js-arrays-objects/Paletas/js/main.js
+++ Módulo-2/Ejercicios-extra-js-arrays-objects/Paletas/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const palettesContainer = document.querySelector('.js-palettes-container');
 let palettes = [];
+let favoritesList = [];
 
 function getData() {
   fetch('https://beta.adalab.es/ejercicios-extra/js-ejercicio-de-paletas/data/palettes.json')
@@ -8,20 +9,21 @@ function getData() {
     .then((data) => {
       console.log(data.palettes);
       palettes = data.palettes;
-      paintPalletes();
+      paintPalettes();
+      listenPalette();
     })
     .catch((err) => {
       console.error('Se ha producido un error:', err);
     });
 }
 
-function paintPalletes() {
+function paintPalettes() {
   let html = '';
-  for (const palette of palettes) {
-    html += `<li>`;
-    html += `<h2 class="palettes__name">${palette.name}</h2>`;
+  for (let i = 0; i < palettes.length; i++) {
+    html += `<li class="js-palette-item palettes__item--favorite " id="${i}">`;
+    html += `<h2 class="palettes__name">${palettes[i].name}</h2>`;
     html += `<div class="palettes__colors">`;
-    for (const color of palette.colors) {
+    for (const color of palettes[i].colors) {
       html += `<div class="palettes__color"style="background-color:#${color}"></div>`;
     }
     html += `</div>`;
@@ -29,4 +31,22 @@ function paintPalletes() {
   }
   palettesContainer.innerHTML = html;
 }
+function listenPalette() {
+  const paletteItems = document.querySelectorAll('.js-palette-item');
+  for (const paletteItem of paletteItems) {
+    paletteItem.addEventListener('click', favoritePalette);
+  }
+}
+function favoritePalette(event) {
+  const clicked = parseInt(event.currentTarget.id);
+  const indexFav = favoritesList.indexOf(clicked);
+  const isFavourite = indexFav !== -1;
+
+  if (isFavourite === false) {
+    favoritesList.push(clicked);
+  } else {
+    favoritesList.splice(indexFav, 1);
+  }
+}
+
 getData();
